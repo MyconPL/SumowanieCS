99867e2c377e4361237fc3e54a6b04ff41f674ce
MyconPL <ziomus12.07@gmail.com>
2024-09-14 00:01:14 +0200

    sumowanie (chui wie czy dziala)

---
+++ sumowanie/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace zliczanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Console.WriteLine("Zaczynamy:");
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbe:");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    result += number;
+                    Console.WriteLine($"Aktualny wynik: {result}");
+                } 
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Wynik końcowy: {result}");
+        }
+    }
+}
